f4f55619a93d6486a94468b19513b500dc89e33a
Eglė Kasperavičiūtė <egle.kasperaviciute.1999@gmail.com>
2022-03-20 14:12:11 +0200

    Citavimo skaiciaus paemimas is GUI

--- view.py
+++ view.py
@@ -4,22 +4,39 @@ from tkinter.filedialog import askopenfile
 
 root = Tk()
 root.title('Moksliniu straipsniu kokybes vertinimo sistema')
+root.geometry('500x200')
 
 
-#opening file ONLY PDF, MOVE TO OTHER FILE
+# opening file ONLY PDF, MOVE TO OTHER FILE
 def open_file():
-    file_path = askopenfile(
-        mode='r', title="Ikelti PDF",  filetypes=(
+    file = askopenfile(
+        mode='r', title="Ikelti PDF", filetypes=(
             ('PDF failai', '*.pdf'),
-            #('Visi failai', '*.*')
         )
     )
-    if file_path is not None:
-        pass
+    if file is not None:
+        # define actions what to do when file is opened
+        content = file.read()
+
+        # pass
+
+
+def get_data():
+    citations = citavimo_var.get()
+    print(citations)
+    citavimo_var.set("")
 
 
 frm = ttk.Frame(root, padding=10)
 frm.grid()
+
+citavimo_var = StringVar()
+
 ttk.Label(frm, text="Ikelkite mokslini straipsni").grid(column=0, row=0)
-ttk.Button(frm, text="Pasirinkite faila", command=lambda:open_file()).grid(column=1, row=0)
+ttk.Button(frm, text="Pasirinkite faila", command=lambda: open_file()).grid(column=1, row=0)
+ttk.Label(frm, text="Citavimo skaicius").grid(column=0, row=1)
+citavimo_skaicius = ttk.Entry(frm, textvariable=citavimo_var).grid(column=1, row=1)
+
+ttk.Button(frm, text="Apdoroti faila", command=lambda: get_data()).grid(column=1, row=2)
+
 root.mainloop()
