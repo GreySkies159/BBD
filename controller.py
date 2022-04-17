3464469832cc833c155e4e74191b195dbdeb7837
Eglė Kasperavičiūtė <egle.kasperaviciute.1999@gmail.com>
2022-04-17 21:46:50 +0300

    Strukturos istraukimas

--- controller.py
+++ controller.py
@@ -1,47 +1,127 @@
-'''#
-from io import StringIO
-
-from pdfminer.high_level import extract_text_to_fp
-from pdfminer.layout import LAParams
-
-#file= "C:\\Users\eglus\PycharmProjects\BBD\"
-file="C:\\Users\eglus\PycharmProjects\BBD\A Survey on Deep Learning Algorithms, Techniques,and Applications.pdf"
-#file="C:\\Users\eglus\PycharmProjects\BBD\DSM11_01.pdf"
+import pdfplumber
+import math
 
 
-output_string = StringIO()
-with open(file, 'rb') as fin:
-    extract_text_to_fp(fin, output_string, laparams=LAParams(), output_type='html', codec=None)
-print(output_string.getvalue().strip())
+def get_filtered_text(file_to_parse: str):
+    full_text = ''
+    filtered_text = ''
+    font_size = get_most_common_size_from_file(file_to_parse)
+    if_abstract = 0
+
+    with pdfplumber.open(file_to_parse) as pdf:
+        #print(pdf.pages)
+        for i in range(len(pdf.pages)):
+            # TODO change to take all pages
+            page_text = pdf.pages[i].dedupe_chars(tolerance=1)
+
+            if i == 0 or i == 1 or i == 2:
+                if if_abstract == 0:
+                    print(page_text.extract_text())
+                    if_abstract = abstract_of_page(page_text.extract_text())
+
+            # TODO replace by seperate function
+            filtered_text = page_text.filter(
+                lambda obj: (obj["object_type"] == "char" and round(obj["size"], 0) > font_size)
+                            or (
+                                    ('Bold' in str(obj.get('fontname')) or
+                                     'bold' in str(obj.get('fontname')))
+                                    and obj["size"] >= font_size))
+
+            # lambda obj: (obj["object_type"] == "char" and obj["size"] > font_size) or (('Bold' in obj["fontname"] or 'bold' in obj["fontname"]) and obj["size"] >= font_size))
+            full_text = full_text + filtered_text.extract_text()
+
+            page_text.close()
+
+    # print(full_text)
+    # print(type(full_text))
+    return full_text, if_abstract
 
 
-'''
-from pdfminer.high_level import extract_pages
-from pdfminer.layout import LTTextContainer, LTChar, LTLine, LAParams
-from collections import Counter
+def get_most_common_size_from_file(pdf_file):
+    font_size_dictionary = {}
+    most_occurent_size_times = 0
+    with pdfplumber.open(pdf_file) as pdf:
+        for i in range(len(pdf.pages)):
+            text = pdf.pages[i]
+            for character in range(len(text.chars)):
+                # if text.chars[character].get('object_type') == 'char':
+                if text.chars[character].get('text') != ' ':
+                    # TODO trunc size to be int
+                    text_size = round(text.chars[character].get('size'), 0)
+                    # TODO temp output
+                    # text_font = text.chars[character].get('fontname')
+                    # if("bold" in text_font or "Bold" in text_font):
+                    # print(text_font)
+                    # if theres no value for this size
+                    if text_size not in font_size_dictionary.keys():
+                        font_size_dictionary[text_size] = 1
+                    if text_size in font_size_dictionary:
+                        font_size_dictionary[text_size] += 1
+                        if font_size_dictionary[text_size] > most_occurent_size_times:
+                            most_occurent_size_times = font_size_dictionary[text_size]
+        # print(font_size_dictionary)
+        # print(most_occurent_size_times)
+        most_common_size = [k for k, v in font_size_dictionary.items() if v == most_occurent_size_times]
+        # print(most_common_size)
+    return most_common_size[0]
 
 
-def most_frequent_size(List):
-    occurence = Counter(List)
-    return occurence.most_common(1)[0][0]
+def structure_evaluation(filtered_text: str, abstract_score):
+    '''
+    :param filtered_text:
+    :return:
+    '''
+    filtered_text = filtered_text.lower()
+    structure_score = 0
+    optional_structure_score = 0
+    introduction_index = 0
+    conclusion_index = 0
+    references_index = 0
+
+    if abstract_score == 1:
+        structure_score = structure_score +1
+        print("Abstract found")
+    elif 'abstract' in filtered_text:
+        structure_score = structure_score + 1
+        print("abstract found")
+    if 'introduction' in filtered_text:
+        introduction_index = filtered_text.find('introduction')
+        structure_score = structure_score + 1
+        print("Introduction found")
+    if 'conclusion' in filtered_text:
+        conclusion_index = filtered_text.find('conclusion')
+        structure_score = structure_score + 1
+        print("Conclusion found")
+    if 'references' in filtered_text:
+        references_index = filtered_text.find('references')
+        structure_score = structure_score + 1
+        print("References found")
+
+    if introduction_index != -1 and  (conclusion_index != -1 or references_index !=-1):
+        if conclusion_index - introduction_index > 0 or references_index - introduction_index > 0:
+            structure_score = structure_score +1
+            print("Methods found")
+
+    optional_structure_score = optional_structure(filtered_text)
+
+    return structure_score
 
 
-PDF_file = "C:\\Users\eglus\PycharmProjects\BBD\A Survey on Deep Learning Algorithms, Techniques,and Applications.pdf"
-#PDF_file ="C:\\Users\eglus\PycharmProjects\BBD\DSM11_01.pdf"
+def optional_structure(page_text : str):
+    if 'acknowledgments' in page_text or 'discussion':
+        print("optional structure found")
+        return 1
+    return 0
 
-Extract_Data = []
-font_sizes = []
-for page_layout in extract_pages(PDF_file):
-    for element in page_layout:
-        if isinstance(element, LTTextContainer):
-            for text_line in element:
-                for character in text_line:
-                    if isinstance(character, LTChar):
-                        Font_size = character.size
-                        #getting array of the font sizes in the file
-                        font_sizes.append(Font_size)
-                Extract_Data.append([Font_size, (element.get_text())])
-#print(font_sizes)
-most_frequent_font_size=most_frequent_size(font_sizes)
-print(most_frequent_font_size)
-print(Extract_Data)
+
+def abstract_of_page(page_text : str):
+    abstract = -1
+    page_text = page_text.lower()
+    '''
+    used for structure analysis on first few pages
+    :return:
+    '''
+    if 'abstract' in page_text:
+        abstract = 1
+        return abstract
+
--- view.py
+++ view.py
@@ -1,30 +1,46 @@
 from tkinter import *
 from tkinter import ttk
-from tkinter.filedialog import askopenfile
+#TODO askopenfilename - returns the path of selected file. you can use that path later to open file
+from tkinter.filedialog import askopenfilename
+
+import controller as con
 
 root = Tk()
 root.title('Moksliniu straipsniu kokybes vertinimo sistema')
 root.geometry('500x200')
+opened_pdf_file_location = ""
 
 
 # opening file ONLY PDF, MOVE TO OTHER FILE
 def open_file():
-    file = askopenfile(
-        mode='r', title="Ikelti PDF", filetypes=(
+    global opened_pdf_file_location
+    file = askopenfilename(
+         title="Ikelti PDF", filetypes=(
             ('PDF failai', '*.pdf'),
         )
     )
     if file is not None:
         # define actions what to do when file is opened
-        content = file.read()
+        # content = file.read()
 
+        opened_pdf_file_location= file
+        print(opened_pdf_file_location)
         # pass
 
 
 def get_data():
+    global opened_pdf_file_location
+    abstract = 0
     citations = citavimo_var.get()
     print(citations)
     citavimo_var.set("")
+    #(opened_pdf_file_location)
+    content, abstract = con.get_filtered_text(opened_pdf_file_location)
+    #print(content)
+
+    structure_score= con.structure_evaluation(content, abstract)
+    print(structure_score)
+    opened_pdf_file_location = ""
 
 
 frm = ttk.Frame(root, padding=10)
